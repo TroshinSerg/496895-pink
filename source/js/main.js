07aca50a20819a7aec78300ef58f36f9ba534772
serge-troshin <serge-troshin@yandex.ru>
2018-11-20 20:42:52 +0300

    сетки, главная страница

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,5 @@
 var html = document.querySelector("html");
+var mainHeader = document.querySelector(".main-header");
 var mainNavInner = document.querySelector(".main-nav__inner");
 var mainNavToggle = document.querySelector(".main-nav__toggle");
 var siteNav = document.querySelector(".site-nav");
@@ -6,6 +7,7 @@ var siteNav = document.querySelector(".site-nav");
 html.classList.remove("no-js");
 
 mainNavToggle.addEventListener("click", function (evt) {
+  mainHeader.classList.toggle("main-header--opened");
   mainNavInner.classList.toggle("main-nav__inner--opened");
   siteNav.classList.toggle("site-nav--opened");
   mainNavToggle.classList.toggle("main-nav__toggle--opened");
